110e132f6262d7a03bb1abdfd66abda4d5775b3e
tejitha896 <tejutejitha87@gmail.com>
2025-07-04 11:16:16 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,24 @@
+function myMenuFuction(){
+    var menuBtn=document.getElementById("myNavMenu");
+    if(menuBtn.className==="nav-menu"){
+        menuBtn.className+="nav-menu";
+    }
+    else{
+        menuBtn.className="nav-menu";
+    }
+}
+
+window.onscroll=function(){headerShadow()};
+function headerShadow(){
+    const navHeader=document.getElementById("header");
+    if(document.body.scrollTop>50||document.documentElement.scrollTop>50){
+        navHeader.style.boxShadow="0 1px 6px rgba(0,0,0,0.1)";
+        navHeader.style.height="70px";
+        navHeader.style.lineHeight="70px";
+    }
+    else{
+        navHeader.style.boxShadow="none";
+        navHeader.style.height="90px";
+        navHeader.style.lineHeight="90px";
+    }
+}
